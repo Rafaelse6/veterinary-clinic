321c52f0899b0b37e8a44e6ac6bbb5e759d2d6de
Rafael Santos <rafa.05125@gmail.com>
2023-06-12 19:31:46 -0300

    Fix: missing annotations

--- animal/src/main/java/org/rafaelsantos/entities/Animal.java
+++ animal/src/main/java/org/rafaelsantos/entities/Animal.java
@@ -1,9 +1,15 @@
 package org.rafaelsantos.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_animals")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Animal {
 
     @Id
