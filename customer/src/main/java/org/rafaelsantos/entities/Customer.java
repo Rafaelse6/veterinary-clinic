d29e5ebca07bb6003c326b0094667fc286c38e59
Rafael Santos <rafa.05125@gmail.com>
2023-06-21 18:31:50 -0300

    Customer entity, dto

---
+++ customer/src/main/java/org/rafaelsantos/entities/Customer.java
@@ -0,0 +1,17 @@
+package org.rafaelsantos.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+}
