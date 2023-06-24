4f5445353f8659bd1ae1de8acabc77cc96341523
Rafael Santos <rafa.05125@gmail.com>
2023-06-24 16:47:16 -0300

    Customer controller

--- customer/src/main/java/org/rafaelsantos/controllers/CustomerController.java
+++ customer/src/main/java/org/rafaelsantos/controllers/CustomerController.java
@@ -1,2 +1,70 @@
-package org.rafaelsantos.controllers;public class CustomerController {
+package org.rafaelsantos.controllers;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.rafaelsantos.dto.CustomerDTO;
+import org.rafaelsantos.services.CustomerService;
+
+import java.util.List;
+
+@Path(value = "/api/customers")
+public class CustomerController {
+
+    @Inject
+    CustomerService customerService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CustomerDTO> findAllCustomers() {
+        return customerService.findAllCustomers();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CustomerDTO findCustomerById(@PathParam("id") Long id) {
+        return customerService.findCustomerById(id);
+    }
+
+    @POST
+    @Transactional
+    public Response saveCustomer(CustomerDTO customerDTO) {
+        try {
+            customerService.createNewCustomer(customerDTO);
+            return Response.ok().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response updateCustomer(@PathParam("id") Long id, CustomerDTO customerDTO) {
+        try {
+            customerService.updateCustomer(id, customerDTO);
+            return Response.accepted().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deleteCustomer(@PathParam("id") Long id) {
+        try {
+            customerService.deleteCustomer(id);
+            return Response.accepted().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+
+    }
 }
