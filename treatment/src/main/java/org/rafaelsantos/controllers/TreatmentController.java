92cb20efb63dc406c02513707759fe1d913ee0c3
Rafael Santos <rafa.05125@gmail.com>
2023-06-26 10:43:33 -0300

    Treatment controller

---
+++ treatment/src/main/java/org/rafaelsantos/controllers/TreatmentController.java
@@ -0,0 +1,41 @@
+package org.rafaelsantos.controllers;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.rafaelsantos.dto.TreatmentDTO;
+import org.rafaelsantos.services.TreatmentService;
+
+import java.util.List;
+
+@Path("/api/orders")
+public class TreatmentController {
+
+    @Inject
+    private TreatmentService treatmentService;
+
+    @GET
+    @RolesAllowed("admin")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<TreatmentDTO> getAllTreatments(){
+        return treatmentService.getAllTreatments();
+    }
+
+    @POST
+    @Transactional
+    public Response saveNewTreatment(TreatmentDTO treatmentDTO){
+        try {
+            treatmentService.saveNewTreatment(treatmentDTO);
+            return Response.ok().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+}
